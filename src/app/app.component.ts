3ba36b95b34dbaaa1012b4bd86983199976199e4
JuanRNS <juanramalho90@gmail.com>
2024-11-06 23:47:08 -0300

    segundo-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TelaPrincipalComponent } from './components/Header-principal/tela-principal.component';
 import { PaginaPrincipalComponent } from './components/pagina-principal/pagina-principal.component';
+import { ProdutosServicosComponent } from './components/produtos-servicos/produtos-servicos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TelaPrincipalComponent,PaginaPrincipalComponent],
+  imports: [RouterOutlet, TelaPrincipalComponent,PaginaPrincipalComponent,ProdutosServicosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
