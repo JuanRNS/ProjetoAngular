2a921ebfe782fbf809be3b2ce2743b8df503038f
JuanRNS <juanramalho90@gmail.com>
2024-11-09 14:26:58 -0300

    atualização para o main

--- src/app/pages/pagina-principal/pagina-principal.component.ts
+++ src/app/pages/pagina-principal/pagina-principal.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { ProdutosServicosComponent } from '../../components/produtos-servicos/produtos-servicos.component';
 import { FormsContatoComponent } from '../../components/forms-contato/forms-contato.component';
-import { TelaPrincipalComponent } from '../../components/header-principal/tela-principal.component';
+import { AmericaCafeComponent } from '../../components/america-cafe/america-cafe.component';
+import { OndeEstamosComponent } from '../../components/onde-estamos/onde-estamos.component';
+import { TelaPrincipalComponent } from '../../components/Header-principal/tela-principal.component';
 
 @Component({
   selector: 'app-pagina-principal',
   standalone: true,
-  imports: [ProdutosServicosComponent,FormsContatoComponent,TelaPrincipalComponent],
+  imports: [ProdutosServicosComponent,FormsContatoComponent,TelaPrincipalComponent,AmericaCafeComponent,OndeEstamosComponent],
   templateUrl: './pagina-principal.component.html',
   styleUrl: './pagina-principal.component.css'
 })
