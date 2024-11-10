fe11b8a565a5d7fe01b150f4f754e0fbcf3e3216
JuanRNS <juanramalho90@gmail.com>
2024-11-09 22:22:46 -0300

    adicionado marcador

--- src/app/components/onde-estamos/onde-estamos.component.ts
+++ src/app/components/onde-estamos/onde-estamos.component.ts
@@ -11,14 +11,14 @@ import {environment as env} from '../../../environments/environment';
 export class OndeEstamosComponent implements OnInit,OnDestroy {
   mapa: any;
 
-  constructor() {}
-
   ngOnInit(): void {
       this.mapa = L.map('mapa').setView([-8.31134802594188, -36.03589714511856], 30);
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          maxZoom: 18
+          maxZoom: 18,
       }).addTo(this.mapa);
+      L.marker([-8.31134802594188, -36.03589714511856]).addTo(this.mapa);
   }
+
 
   ngAfterViewInit(): void {
       this.mapa.invalidateSize();
