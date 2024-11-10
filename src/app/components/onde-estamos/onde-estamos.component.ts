e83eb59ced3452e7cfb7453f1c682684ed223dd8
JuanRNS <juanramalho90@gmail.com>
2024-11-09 22:10:16 -0300

    adicionando imagem de mapa

--- src/app/components/onde-estamos/onde-estamos.component.ts
+++ src/app/components/onde-estamos/onde-estamos.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import {environment as env} from '../../../environments/environment';
 @Component({
   selector: 'app-onde-estamos',
   standalone: true,
@@ -7,6 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './onde-estamos.component.html',
   styleUrl: './onde-estamos.component.css'
 })
-export class OndeEstamosComponent {
+export class OndeEstamosComponent implements OnInit,OnDestroy {
+  mapa: any;
 
+  constructor() {}
+
+  ngOnInit(): void {
+      this.mapa = L.map('mapa').setView([-8.31134802594188, -36.03589714511856], 30);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 18
+      }).addTo(this.mapa);
+  }
+
+  ngAfterViewInit(): void {
+      this.mapa.invalidateSize();
+  }
+
+  ngOnDestroy(): void {
+      this.mapa.remove();
+  }
 }
