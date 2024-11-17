810e2f1f8c0a8dccbd408cfe201b276a60923ba9
Victor <victor_hugo_lacerda@hotmail.com>
2024-11-17 17:09:53 -0400

    finalização do footer, ficando pendente apenas a troca de alguns icon, pois o numero de downloads free acabou e a responsividade da pagina.'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { PaginaPrincipalComponent } from './pages/pagina-principal/pagina-princi
 import { ProdutosServicosComponent } from './components/produtos-servicos/produtos-servicos.component';
 import { FormsContatoComponent } from './components/forms-contato/forms-contato.component';
 import { MapasComponent } from './pages/mapas/mapas.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,PaginaPrincipalComponent,MapasComponent],
+  imports: [RouterOutlet,PaginaPrincipalComponent,MapasComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/pagina-principal/pagina-principal.component.ts
+++ src/app/pages/pagina-principal/pagina-principal.component.ts
@@ -5,11 +5,12 @@ import { AmericaCafeComponent } from '../../components/america-cafe/america-cafe
 import { TelaPrincipalComponent } from '../../components/Header-principal/tela-principal.component';
 import { MapasComponent } from '../mapas/mapas.component';
 import { RederizaMapasComponent } from "../../components/rederiza-mapas/rederiza-mapas.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-pagina-principal',
   standalone: true,
-  imports: [ProdutosServicosComponent, FormsContatoComponent, TelaPrincipalComponent, AmericaCafeComponent, MapasComponent],
+  imports: [ProdutosServicosComponent, FormsContatoComponent, TelaPrincipalComponent, AmericaCafeComponent, MapasComponent, FooterComponent],
   templateUrl: './pagina-principal.component.html',
   styleUrl: './pagina-principal.component.css'
 })
