5895eacdb9a639b660d3b485d4e225b650bcb861
felipegasanto <felipegabrielscontato@gmail.com>
2024-11-21 15:35:22 -0300

    aba de promoções não pronta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { PaginaPrincipalComponent } from './pages/pagina-principal/pagina-princi
 import { ProdutosServicosComponent } from './components/produtos-servicos/produtos-servicos.component';
 import { FormsContatoComponent } from './components/forms-contato/forms-contato.component';
 import { MapasComponent } from './pages/mapas/mapas.component';
+import { PromocoesComponent } from './components/promocoes/promocoes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,PaginaPrincipalComponent,MapasComponent],
+  imports: [RouterOutlet,PaginaPrincipalComponent,MapasComponent, PromocoesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
