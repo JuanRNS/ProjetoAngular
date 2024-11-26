adbe97e5fc57b73fd84fd79550ca51b503922e91
JuanRNS <juanramalho90@gmail.com>
2024-11-25 22:14:41 -0300

    commit ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { PaginaPrincipalComponent } from './pages/pagina-principal/pagina-princi
 import { FormsContatoComponent } from './components/forms-contato/forms-contato.component';
 import { MapasComponent } from './pages/mapas/mapas.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { TelaPrincipalComponent } from './components/Header-principal/tela-principal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,PaginaPrincipalComponent,MapasComponent,FooterComponent],
+  imports: [RouterOutlet,PaginaPrincipalComponent,MapasComponent,FooterComponent,TelaPrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
