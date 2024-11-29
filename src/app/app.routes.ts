affc7d0b41a1ff63de31165ec6b0d83984d628f7
felipegasanto <felipegabrielscontato@gmail.com>
2024-11-29 11:00:11 -0300

    Botão estilizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { LeafletMapComponent } from './components/leaflet-map/leaflet-map.component';
 import { PaginaPrincipalComponent } from './pages/pagina-principal/pagina-principal.component';
 import { RederizaMapasComponent } from './components/rederiza-mapas/rederiza-mapas.component';
+import { PromocoesComponent } from './components/promocoes/promocoes.component';
 
 export const routes: Routes = [
   { path: '', component: PaginaPrincipalComponent },
-  { path: 'mapa', component: RederizaMapasComponent }
+  { path: 'mapa', component: RederizaMapasComponent },
+  { path: 'promocoes', component: PromocoesComponent },
 ];
--- src/app/pages/pagina-principal/pagina-principal.component.ts
+++ src/app/pages/pagina-principal/pagina-principal.component.ts
@@ -4,14 +4,23 @@ import { TelaPrincipalComponent } from '../../components/Header-principal/tela-p
 import { MapasComponent } from '../mapas/mapas.component';
 import { RederizaMapasComponent } from "../../components/rederiza-mapas/rederiza-mapas.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { Router, RouterOutlet } from '@angular/router';
+import { PromocoesComponent } from '../../components/promocoes/promocoes.component';
 
 @Component({
   selector: 'app-pagina-principal',
   standalone: true,
-  imports: [ FormsContatoComponent, TelaPrincipalComponent, MapasComponent],
+  imports: [ FormsContatoComponent, TelaPrincipalComponent, MapasComponent, RouterOutlet],
   templateUrl: './pagina-principal.component.html',
   styleUrl: './pagina-principal.component.css'
 })
 export class PaginaPrincipalComponent {
+  constructor(private router: Router) {
+
+  }
+
+  promocoes(){
+    this.router.navigate(['promocoes'])
+  }
 
 }
