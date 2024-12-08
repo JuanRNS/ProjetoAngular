a67d4b660064fb004865882c50f37c0073fbdeaf
Victor <victor_hugo_lacerda@hotmail.com>
2024-12-08 10:39:55 -0400

    subindo login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,11 @@ import { RederizaMapasComponent } from './components/rederiza-mapas/rederiza-map
 import { FormsContatoComponent } from './components/forms-contato/forms-contato.component';
 import { FormsTrabalheComponent } from './components/forms-trabalhe/forms-trabalhe.component';
 import { PromocoesComponent } from './components/promocoes/promocoes.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: PaginaPrincipalComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'forms-trabalhe', component: FormsTrabalheComponent },
   { path: 'mapa', component: RederizaMapasComponent },
   { path: 'promocoes', component: PromocoesComponent },
